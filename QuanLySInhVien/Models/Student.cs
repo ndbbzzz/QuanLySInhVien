ef6879adb09b5c81fbad6399cb974880caccd4de
binh <ndbb7112006@gmail.com>
2026-08-14 14:45:59 +0700

    student constructor

---
+++ QuanLySInhVien/Models/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuanLySInhVien.Models
+{
+    public class Student
+    {
+        public string MaSinhVien { get; set; } = "";
+        public string HoTen { get; set; } = "";
+        public DateTime NgaySinh { get; set; }
+        public string GioiTinh { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string SoDienThoai { get; set; } = "";
+        public string NganhHoc { get; set; } = "";
+        public double DiemTrungBinh { get; set; }
+        public string TrangThaiHocTap { get; set; } = "";
+
+        public Student() { }
+        public Student(
+         string maSinhVien,
+         string hoTen,
+         DateTime ngaySinh,
+         string gioiTinh,
+         string email,
+         string soDienThoai,
+         string nganhHoc,
+         double diemTrungBinh,
+         string trangThaiHocTap)
+        {
+            MaSinhVien = maSinhVien;
+            HoTen = hoTen;
+            NgaySinh = ngaySinh;
+            GioiTinh = gioiTinh;
+            Email = email;
+            SoDienThoai = soDienThoai;
+            NganhHoc = nganhHoc;
+            DiemTrungBinh = diemTrungBinh;
+            TrangThaiHocTap = trangThaiHocTap;
+        }
+    }
+}
